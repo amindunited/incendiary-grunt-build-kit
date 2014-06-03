efcd1d8c0fd65b419ae8d1388641d7547c03f55a
amindunited <buckley.robin@gmail.com>
2014-06-03 16:08:55 +1000

    Fixed up tasks for grunt watch

--- tasks/options/watch.js
+++ tasks/options/watch.js
@@ -6,37 +6,49 @@ module.exports =  {
     	}
     },
 
+	hbs: {
+		files: ['<%= paths.source %>/**/*.hbs'],
+		tasks: ['clean:build',
+			'env:development', 
+			'preprocess:development', 
+			'transpile:app',
+			'jshint',
+			'less:development',
+			'emberhandlebars',
+			'concat_sourcemap:development',
+			'copy:images_development',
+			'clean:temp'
+		]
+	},
+
 	js: {
 		files: ['<%= paths.source %>/**/*.js', '!<%= paths.source %>/vendor/**/*.js'],
-		tasks: ['transpile:app']
+		tasks: ['clean:build',
+			'env:development', 
+			'preprocess:development', 
+			'transpile:app',
+			'jshint',
+			'less:development',
+			'emberhandlebars',
+			'concat_sourcemap:development',
+			'copy:images_development',
+			'clean:temp'
+		]
 	},
 	
 	less: {
 		files: 'app/less/**/*.less',
-		tasks: ['less:development']
-	},
+		tasks: ['clean:build',
+			'env:development', 
+			'preprocess:development', 
+			'transpile:app',
+			'jshint',
+			'less:development',
+			'emberhandlebars',
+			'concat_sourcemap:development',
+			'copy:images_development',
+			'clean:temp'
+		]
+	}
 
-	// core: {
-	// 	files: 'app/libs/**/*.js',
-	// 	tasks: ['uglify:jsLibs_dev']
-	// },
-
-	// app: {
-	// 	files: 'app/src/**/*.js',
-	// 	tasks: ['uglify:app_dev']
-	// },
-
-	// templates: {
-	// 	files: 'app/src/**/*.hbs',
-	// 	tasks: ['emberTemplates']
-	// },
-
-	// less_server: {
-	// 	files: ['css/index.css', 'js/libs.js', 'js/index.js', 'js/templates.js'],
-	// 	options: {
-	// 		livereload: {
-	// 			port: 8999
-	// 		}
-	// 	}
-	// }
 };
