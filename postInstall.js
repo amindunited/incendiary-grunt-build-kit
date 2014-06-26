2678801e800417aad0e9d283395b81167532764b
amindunited <buckley.robin@gmail.com>
2014-06-26 15:46:03 +1000

    Added Cordova config and postInstall tasks

--- postInstall.js
+++ postInstall.js
@@ -7,7 +7,7 @@ var Handlebars = require('handlebars');
 var child_process = require('child_process');
 var exec = require('child_process').exec;
 
-
+var cordova_config;
 var projectConfig;
 var localConfig;
 var applicationFile;
@@ -21,6 +21,11 @@ var defaultFilesTemplates = [
 	},
 	{
 		"file": "project.config.json",
+		"src_dir": "./post_install_files/",
+		"dest_dir": "./",
+	},
+	{
+		"file": "cordova.config.json",
 		"src_dir": "./post_install_files/",
 		"dest_dir": "./",
 	},
@@ -62,17 +67,90 @@ copyDefaultFiles();
 /* EOF Copy Missing Default Files */
 
 
-/* SOF Run Bower install */
-
-var runCMD = function(cmdStr, callback){
+var runCMD = function(cmdStr, options, callback){
 	
-	var cmd = child_process.spawn('bower', ['install'], { stdio: 'inherit' }).on;
+	var cmd = child_process.spawn(cmdStr, options, { stdio: 'inherit' }).on('close', function (code) {
+	  console.log(cmdStr + ' exited with code ' + code);
+	  callback();
+	});;
 
 };
 
-runCMD("bower install");
+
+/* SOF cordova set up */
+
+var cordovaConfigComplete = function(){
+	//change working directory back to rproject root (from cordova/)
+	process.chdir('../');
+}
+
+var cordovaAddPlugins = function() {
+	var plugins_config = cordova_config.plugins;
+	var plugins = [];
+	var callbackArray = [];
+	
+	//Remove disabled plugins from list
+	for (var i = 0; i < plugins_config.length; i++) {
+		if (plugins_config[i].add) {
+			plugins.push(plugins_config[i].registry);
+		}
+	}
+	
+	var loadNextCordovaPlugin = function(index){
+		runCMD("cordova",  ['plugin', 'add', plugins[index]], function(){
+			if ((plugins.length - 1) > index) {
+				loadNextCordovaPlugin(index+1);
+			} else {
+				cordovaConfigComplete();
+			}
+
+		});
+	};
+
+	loadNextCordovaPlugin(0);
+	
+};
+
+var cordovaAddPlatforms = function() {
+
+	process.chdir('./cordova');
+
+	runCMD("cordova",  ['platforms', 'add', 'android'], function(){
+		if (process.platform === 'darwin') {
+			runCMD("cordova",  ['platforms', 'add', 'ios'], function(){
+				cordovaAddPlugins();
+			});
+		} else {
+			cordovaAddPlugins();
+		}
+	});
+
+};
+
+var cordovaCreate = function () {
+	
+	cordova_config = require('./cordova.config.json');
+	
+	if ( !cordova_config.enable ) {
+		console.log("Cordova Has been disabled for this project");
+		return;
+	}
+
+	runCMD("cordova",  ['create', 'cordova', 'com.luxbet.app', 'Luxbet'], cordovaAddPlatforms);
+};
+
+/* EOF cordova set up */
+
+
+
+/* SOF Run Bower install */
+
+runCMD("bower",  ["install"], cordovaCreate);
 
 /* EOF Run Bower install */
+
+
+
 
 
 
