8eafaff91da46bbc225db291258fa96c1d8987ea
amindunited <buckley.robin@gmail.com>
2014-07-16 17:57:26 +1000

    Exclude Helpers from transpiler

--- tasks/options/transpile.js
+++ tasks/options/transpile.js
@@ -24,7 +24,7 @@ module.exports = {
 			{
 				expand: true,
 				cwd: 'app',//Setting this to <%= paths.src %> creates module naming issues
-				src: ['**/*.js', '!<%= paths.config %>', '!*/templates/**/*'],
+				src: ['**/*.js', '!<%= paths.config %>', '!**templates/**', '!**helpers/**'],
 				dest: '<%= paths.temporary %>/transpiled/app',
 				modPath: '<%= paths.ember_modules %>'
 			}
